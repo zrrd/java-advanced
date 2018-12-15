ed6ea6d2cd06600f483a84f47db13724cc70a0fd
shaoyijiong <fantasy9417@163.com>
2018-12-15 14:51:03 +0800

    Initial commit

---
+++ cache-avalanche/src/main/java/cn/learn/cacheavalanche/CacheAvalancheApplication.java
@@ -0,0 +1,14 @@
+package cn.learn.cacheavalanche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheAvalancheApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CacheAvalancheApplication.class, args);
+  }
+
+}
+
