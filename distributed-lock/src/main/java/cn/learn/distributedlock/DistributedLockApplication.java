88c2ea71f66c3a1f2ccea9e5bc366da115313ef1
shaoyijiong <fantasy9417@163.com>
2018-12-16 14:40:01 +0800

    分布式锁

---
+++ distributed-lock/src/main/java/cn/learn/distributedlock/DistributedLockApplication.java
@@ -0,0 +1,14 @@
+package cn.learn.distributedlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedLockApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DistributedLockApplication.class, args);
+  }
+
+}
+
