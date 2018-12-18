b17901e5453a311a94ad5d7549e771997d77fb4f
邵益炯 <shaoyijiong@worken.cn>
2018-12-18 21:01:58 +0800

    feat(秒杀系统):秒杀系统1

---
+++ flash-sale/src/main/java/cn/learn/flashsale/FlashSaleApplication.java
@@ -0,0 +1,14 @@
+package cn.learn.flashsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashSaleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FlashSaleApplication.class, args);
+  }
+
+}
+
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 ### Java 高级知识
 #### 1. cache-avalanche  缓存血崩的解决策略
 #### 2. distributed-lock 实现一个分布式锁
+#### 3. flash-sale 高并发秒杀系统
